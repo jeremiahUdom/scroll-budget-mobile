3cb37093637ccca2e721fc62b946e585f7db0074
jeremiahUdom <116927290+jeremiahUdom@users.noreply.github.com>
2026-07-06 13:26:26 +0100

    Restore app.config.js

---
+++ app.config.js
@@ -0,0 +1,60 @@
+export default {
+  "expo": {
+    "name": "scroll-budget",
+    "slug": "scroll-budget",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "scrollbudget",
+    "userInterfaceStyle": "automatic",
+    "newArchEnabled": true,
+    "android": {
+      "adaptiveIcon": {
+        "backgroundColor": "#1D9E75",
+        "foregroundImage": "./assets/images/android-icon-foreground.png",
+        "backgroundImage": "./assets/images/android-icon-background.png",
+        "monochromeImage": "./assets/images/android-icon-monochrome.png"
+      },
+      "edgeToEdgeEnabled": true,
+      "predictiveBackGestureEnabled": false,
+      "package": "com.jeremiahudom.scrollbudget",
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON,
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#1D9E75",
+        }
+      ],
+      [
+        "expo-font",
+        {
+          "fonts": [
+            "./assets/fonts/Inter_18pt-Bold.ttf",
+            "./assets/fonts/Inter_18pt-SemiBold.ttf",
+            "./assets/fonts/Inter_18pt-Medium.ttf",
+            "./assets/fonts/Inter_18pt-Regular.ttf"
+          ]
+        }
+      ],
+      "@react-native-firebase/app",
+      "@react-native-firebase/auth"
+    ],
+    "experiments": {
+      "typedRoutes": true,
+      "reactCompiler": true
+    },
+    "extra": {
+      "router": {},
+      "eas": {
+        "projectId": "c5fa72af-b779-42dd-aa1b-9e09016cea5b"
+      }
+    },
+    "owner": "jeremiahudom"
+  }
+}
