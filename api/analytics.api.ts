2e3609111f6e0b84f241964a2e861d8179c79a26
jeremiahUdom <116927290+jeremiahUdom@users.noreply.github.com>
2026-07-10 17:20:58 +0100

    Remove /api

--- api/analytics.api.ts
+++
@@ -1,8 +0,0 @@
-import { WeeklyAnalyticsResponse } from "@/types/WeeklyAnalytics";
-import { authApi } from "./client";
-
-export const getWeeklyAnalyticsApi = async (): Promise<WeeklyAnalyticsResponse> => {
-  const response = await authApi.get<{success: boolean, weeklyAnalytics: WeeklyAnalyticsResponse}>("/analytics/me/weekly")
-
-  return response.data.weeklyAnalytics;
-}
